b0035658981da6368b5654beed6a5cd9e0a31585
Aqua <s3976073@rmit.edu.vn>
2024-09-13 23:49:52 +0700

    remove unused function

--- src/cli_function.h
+++ src/cli_function.h
@@ -4,19 +4,7 @@
 // Character defines
 #define EMPTY_CHAR 32
 #define BACKSPACE 8
-
-// Define constants
-#define MAX_CMD_SIZE 100
-#define HISTORY_STORAGE 20
-
-// Function prototypes for CLI operations
-
-// Function for auto completion
-void handle_auto_completion(char* cli_buffer, int index);
-
-// Function to navigate command history
-void navigate_history(int direction, char* cli_buffer, int* index, char cli_buffer_history[HISTORY_STORAGE][MAX_CMD_SIZE], int* cli_buffer_history_index);
-
+ 
 // Function for backspace
 void backspace(char* cli_buffer, int* index);
 
